11d279c06b2e6c1d69fd08722aa8519498cb20ed
이정민 <102846582+Leejungmin1119@users.noreply.github.com>
2025-02-16 21:40:52 +0900

    백준 푼거

--- 파이썬_백준풀이/실버 문제/test.py
+++ 파이썬_백준풀이/실버 문제/test.py
@@ -1,20 +1,52 @@
-#메인함수
+#https://www.acmicpc.net/problem/2630
 
-import heapq
+#재귀함수
 
-n = int(input())
+def roop(length,y,x):
 
-#리스트 생성
-lists = []
+    global bule,white
 
+    ch = [0,0]
 
-for i in range(n):
-    for i in map(int,input().split()):
-        #힙에다가 추가
-        heapq.heappush(lists,i)
+    end_x = x+length 
+    end_y = y+length
 
-        #힙이 길이 n을 넘으면 최솟값을 pop
-        if len(lists) > n:
-            heapq.heappop(lists)
+    # 정사각형 안에 지금 파란색 또는 흰색으로만 구성되어 있는지 확인
+    for i in range(y,end_y):
+        if 1 not in lists[i][x:end_x]:
+                # 화이트
+                ch[0]+=1
+        elif 0 not in lists[i][x:end_x]:
+                ch[1]+=1
 
-print(lists[0])
+    if ch[0] == length:
+        white+=1
+    elif ch[1] == length:
+        bule+=1
+    
+    # 위조건에 안맞으면 완벽한 색종이가 아님 따라서 4분면으로 나눠서 재귀함수를 실행해준다.
+    elif length != 1:
+        
+        # 2사분면으로 이동
+        roop((length//2),y,x)
+
+        # 1사분면으로 이동
+        roop((length//2),y,x+(length//2))
+
+        # 3사분면으로 이동
+        roop((length//2),y + (length//2),x)
+
+        # 4사분면으로 이동
+        roop((length//2),y + (length//2),x+(length//2))
+
+    return 0 
+# 메인함수
+length = int(input())
+bule,white = 0,0
+# 1 = 파란색 , 0 = 흰색
+lists = [list(map(int,input().split())) for i in range(length)]
+
+roop(length,0,0)
+
+print(white)
+print(bule)
