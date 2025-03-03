c3fed953295665acca335e196f922d0f3bfa1391
Leejungmin1119 <leeas3140@naver.com>
2025-03-03 23:21:08 +0900

    백준 푼거

--- study_python/asd.py
+++ study_python/asd.py
@@ -1,30 +1,64 @@
-# 부모 노드를 찾는 함수 (경로 압축 적용)
-man, party = map(int, input().split())
+import sys
+import heapq 
 
-# 진실을 아는 사람의 정보 입력
-a, *real = map(int, input().split())
-real = set(real)
-lists = []
-ans = party
-# 파티 인원수 입력
-for i in range(party):
+def BFS(A):
     
-    lists.append(set(map(int,input().split()[1:])))
-
-#진실을 아는 사람들이랑 같이 들은 사람들 입력
-
-for i in range(man):
-    for j in range(party):
+    lists = []
+    #초기 가격값과 시작지점 저장
+    heapq.heappush(lists,[value_result[start_p],start_p])
+    
+    while lists:
+        v,s = heapq.heappop(lists)
         
-        # 교집합의 값이 존재하는지 확인(있으면 이 파티는 진실을 말해줘야 된다..)
-        if lists[j] & real:
-            real = real.union(lists[j])  
+        #만약 저장된 값보다 더 크면 돌릴필요가 없음
+        if value_result[s] < v:
+            continue
+        
+        for i in bus[s]:
             
-for i in range(party):
-    
-    # 최종적으로 과장되어 말할 수 있는 파티의 갯수를 확인한다.
-    if lists[i] & real:
-        ans -=1
+            if value_result[i[1]] <= (v + i[0]):
+                continue
+            
+            
+            
+            value_result[i[1]] = v+i[0]
+            heapq.heappush(lists,[value_result[i[1]],i[1]])
+        
+N =int(sys.stdin.readline())#버스갯수
+M =int(sys.stdin.readline())#노선갯수
 
-#출력
-print(ans)
+bus = {i : [] for i in range(N+1)} # 노선을 저장하는 딕셔너리 변수
+
+value_result = [float('inf') for i in range(N+1)]#가격
+
+for i in range(M):
+    #노선 정보 입력
+    start,end,value = map(int,sys.stdin.readline().split())
+    
+    bus[start].append([value,end])
+    
+start_p,end_p = map(int,sys.stdin.readline().split())
+
+#시작지점은 0
+value_result[start_p] = 0
+
+BFS(start_p)    
+    
+print(value_result[end_p])
+
+
+# 1. 그냥 이중  for문이 아니라 그냥 1차원 배열로 넣어야 함.
+# 2. while 문을 실행하고 나서 현재 내가 스타트 지점까지 오는데의 가격과 지금 저장되어 있는 최저 비용이랑 비교해야함 만약 false면 아래 함수는 무시한다.
+#(왜 생기는가?) 그 변수들중 스타드 지점이 다른데 목적지가 같은 변수가 여려개 저장되어있다 치면 
+"""
+저장 [비용(10),5] 를 저장
+
+하지만 다음에 변수는 [비용(3),5] 이러면
+
+애초에 처음 변수 [10,5] 자체를 실행할 필요가 없어진다.
+따라서 무의미한 시간소모가 되기에 따로 비교하는 if문을 구현해야 한다.
+
+2. 그러면 작은 값이 나오면 그 값을 저장해야 하나?
+
+필요없다. value_result[s]은 v보다 같거나 작다.
+"""
--- study_python/solve test.py
+++ study_python/solve test.py
@@ -1,22 +1,37 @@
-import sys
-input = sys.stdin.readline
+import heapq
+def BFS(A):
+    global N,M
+    
+    que = [A]
+    
+    while que:
+        check_list = heapq.heappop(que)
+        
+        for i in bus[check_list]:# i = 버스의 도착지
+            ch = value_list[1][check_list] + value_list[check_list][i]
+            if value_list[1][i] > ch:
+                
+                value_list[1][i] = ch
+                heapq.heappush(que,i)
+#메인함수
 
-n, m = map(int, input().split())
-knowList = set(input().split()[1:])
-parties = []
+N = int(input())# 지점 갯수
+M = int(input())# 버스 위치
+bus = {i : [] for i in range(N+1)}
+value_list = [[float('inf')] * (N+1) for i in range(N+1)]
+for i in range(M):
+    start,end,value = map(int,input().split())
+    
+    
+    value_list[start][end] = value# 비용 정보 저장
+    value_list[end][start] = value
+    
+    bus[start].append(end)# 버스 정보 저장
+    bus[end].append(start)
 
-for _ in range(m):
-    parties.append(set(input().split()[1:]))
 
-for _ in range(m):
-    for party in parties:
-        if party & knowList:
-            knowList = knowList.union(party)
+start_point,end_point = map(int,input().split())
 
-cnt = 0
-for party in parties:
-    if party & knowList:
-        continue
-    cnt += 1
+BFS(start_point)
 
-print(cnt)
+print(value_list[start_point][end_point])   
